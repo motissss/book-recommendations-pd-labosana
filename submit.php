cc7b6e9c9d0a4ac687e8cc63ee0acf86c830754d
daniels <danielsv2008@gmail.com>
2025-12-11 14:03:09 +0200

    izveidots savienojums ar MySQL un uztaisīta tabula

--- submit.php
+++ submit.php
@@ -1,4 +1,13 @@
 <?php
+$host = "localhost";
+$user = "book_review_user_24092025";
+$pass = "password";
+$dbname = "book_review_24092025";
 
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
 <a href="index.html">Atpakaļ</a>
